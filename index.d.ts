7e47c1c2fab30e54a26eb583ca4838b3b96b131b
xiaoyong <454926339@qq.com>
2020-11-09 15:36:43 +0800

    Fix some types error

--- index.d.ts
+++ index.d.ts
@@ -2,17 +2,17 @@ export interface XYEvent {
     type: string;
 }
 
-export interface Lisenter<T> {
+export interface Listener<T> {
     (event: T & XYEvent): void;
 }
 
 declare class Events {
     constructor();
 
-    addEventListener<T>(type: string, lisenter: Lisenter<T>): void;
-    on<T>(type: string, lisenter: Lisenter<T>): void;
-    removeEventListener<T>(type: string, lisenter: Lisenter<T>): void;
-    off<T>(type: string, lisenter: Lisenter<T>): void;
+    addEventListener<T>(type: string, lisenter: Listener<T>): void;
+    on<T>(type: string, lisenter: Listener<T>): void;
+    removeEventListener<T>(type: string, lisenter: Listener<T>): void;
+    off<T>(type: string, lisenter: Listener<T>): void;
 
     dispatchEvent<T>(event: T & XYEvent): boolean;
     dispatchEvent<T>(type: string, ...args): boolean;
